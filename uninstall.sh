9ab83dc3b6b128bf570c8216bd49b4d1a8db993c
Piotr Bajdek <96956242+piotrbajdek@users.noreply.github.com>
2022-08-03 06:46:23 +0200

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+# Apache License 2.0
+# Copyright 2022 Piotr Bajdek
+
+#!/usr/bin/env bash
+
+sudo rm /var/usrlocal/bin/silverpin0
+sudo rm /var/usrlocal/bin/silverpin1
+sudo rm /var/usrlocal/bin/silverunpin0
+sudo rm /var/usrlocal/bin/silverunpin1
+sudo rm /var/usrlocal/bin/silverunpin2
+sudo rm /var/usrlocal/bin/silverunpin3
